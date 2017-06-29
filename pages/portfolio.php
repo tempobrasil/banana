a38d5dc77e39c4ba5dd94b6fe9d5db17ab68e8d0
Tihh Gonçalves <tihh.gon@gmail.com>
2017-06-29 12:25:17 -0300

    potsoflio

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -9,27 +9,60 @@ includeHeader();
             <h1>Portfólio</h1>
             <div class="page">
 
+                <p>Conheça abaixo um pouco do nosso trabalho. Aproveite e entre em contato com  nossa equipe clicando no botão do chat. Estamos prontos pra atender você!</p>
+
                 <h3>Web</h3>
 
-                <div id="gallery" style="display:none;">
+                <div id="galeria_web" style="display:none;">
 
                     <?
-                    $webs = array();
 
-                    $web = array(
-                        'mini'  => '',
-                        'image' => '',
-                        'title' => ''
-                    );
-                    $webs[] = $web;
+                    $json_file = file_get_contents(get_config('SITE_PATH') . 'arquivos_portfolio/portfolio.json');
+                    $json_str = json_decode($json_file, true);
+
+                    $pecas_web = $json_str['web'];
+                    shuffle($pecas_web);
+
+                    foreach($pecas_web as $peca) {
+
+                        ?>
+                        <a href="http://unitegallery.net">
+                            <img alt="<?= $peca['title']; ?>"
+                                 src="<?= get_config('SITE_URL') . 'arquivos_portfolio/' . $peca['thumb']; ?>"
+                                 data-image="<?= get_config('SITE_URL') . 'arquivos_portfolio/' . $peca['image']; ?>"
+                                 data-description="<?= $peca['title']; ?>"
+                                >
+                        </a>
+                    <?
+                    }
                     ?>
-                    <a href="http://unitegallery.net">
-                        <img alt="Blog Pessoal de Tihh Gonçalves"
-                             src="http://work.tiago.art.br/wp-content/uploads/2015/12/tiago_fotoprincipal.jpg"
-                             data-image="http://work.tiago.art.br/wp-content/uploads/2015/12/tiago_fotoprincipal.jpg"
-                             data-description="This is a Lemon Slice"
-                             >
-                    </a>
+
+
+
+                </div>
+
+                <h3>Design</h3>
+
+                <div id="galeria_design" style="display:none;">
+
+                    <?
+
+                    $pecas_design = $json_str['design'];
+                    shuffle($pecas_design);
+
+                    foreach($pecas_design as $peca) {
+
+                        ?>
+                        <a href="http://unitegallery.net">
+                            <img alt="<?= $peca['title']; ?>"
+                                 src="<?= get_config('SITE_URL') . 'arquivos_portfolio/' . $peca['thumb']; ?>"
+                                 data-image="<?= get_config('SITE_URL') . 'arquivos_portfolio/' . $peca['image']; ?>"
+                                 data-description="<?= $peca['title']; ?>"
+                                >
+                        </a>
+                    <?
+                    }
+                    ?>
 
 
 
@@ -51,12 +84,18 @@ includeFoot();
 
     $(document).ready(function(){
 
-        $("#gallery").unitegallery({
+        $("#galeria_web").unitegallery({
+            gallery_theme: "tiles",
+            tiles_type: "nested"
+        });
+
+        $("#galeria_design").unitegallery({
             gallery_theme: "tiles",
             tiles_type: "nested"
         });
     });
 
 
-
+    /* Abre Chat */
+    iniciarChat('Estava olhando o portfólio da Vitamina e queria conversar com vocês.');
 </script>
