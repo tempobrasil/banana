5863015d955594ddec74850aa04ecab0be314455
Tihh Gonçalves <tihh.gon@gmail.com>
2017-06-30 14:58:24 -0300

    up

--- includes/inc.site.foot.php
+++ includes/inc.site.foot.php
@@ -12,7 +12,7 @@ if(!is_localhost()) {
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-101933054-1', 'auto');
+        ga('create', 'UA-101933054-1', { 'userId': '12345' });
         ga('send', 'pageview');
 
     </script>
